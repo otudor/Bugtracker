5bdc5e991c5383168c9618a92facacaa6a80236c
otudor <lord_scorpio2005@yahoo.com>
2015-02-01 12:32:39 +0200

    added UI components

---
+++ src/main/java/com/endava/bugtracker/util/SecurityUtil.java
@@ -0,0 +1,35 @@
+package com.endava.bugtracker.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.endava.bugtracker.domain.User;
+import com.endava.bugtracker.service.UserDetailsDto;
+
+public class SecurityUtil {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SecurityUtil.class);
+
+    public static void logInUser(User user) {
+        LOGGER.info("Logging in user: {}", user);
+
+        UserDetailsDto userDetails = UserDetailsDto.getBuilder()
+                .firstName(user.getFirstName())
+                .id(user.getId())
+                .lastName(user.getLastName())
+                .password(user.getPassword())
+                .role(user.getRoles())
+                .socialSignInProvider(user.getSignInProvider())
+                .username(user.getEmail())
+                .build();
+        LOGGER.debug("Logging in principal: {}", userDetails);
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        LOGGER.info("User: {} has been logged in.", userDetails);
+    }
+}
